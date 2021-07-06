266707e5baf8756fe154394d93911e294fff1d2c
Dev-Elie <eochieng9448@gmail.com>
2021-07-06 19:03:54 +0300

    mpesa stk-push

--- mpesa/pay.py
+++ mpesa/pay.py
@@ -24,9 +24,10 @@ from flask_login import (
 )
 import requests
 import json
+import base64
 from requests.auth import HTTPBasicAuth
 import unicodedata, re, itertools, sys
-
+from datetime import datetime
 
 mpesa = Blueprint("mpesa", __name__, url_prefix="/mpesa")
 
@@ -34,14 +35,13 @@ mpesa = Blueprint("mpesa", __name__, url_prefix="/mpesa")
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
 # *** Authorization Request in Python ***|
-
-# mpesa details
-
-consumer_key = 'rKCtIJJqQF8fU3Chw508Bl5JMCwIh5xn'
-consumer_secret = 'Bi4gdEUCOmPpmkcx'
 base_url = 'http://102.167.16.174'
+consumer_key = 'Y029iJrJGeGdOlBKIlhGgJnrL6AAGrtF'
+consumer_secret = 'HF2s63AZ5RTw00jT'
+business_short_code = '5983411'
+lipa_na_mpesa_passkey = ''
+
 
 # access token
 @mpesa.route("/access_token")
@@ -99,6 +99,13 @@ def validate():
         "ResultDesc": "Accepted"
     }
 
+date = datetime.now()
+formatted_time = date.strftime("%Y%m%d%H%M%S")
+
+data_to_encode = business_short_code + lipa_na_mpesa_passkey + formatted_time
+encoded_string = base64.b64encode(data_to_encode.encode())
+decoded_string = encoded_string.decode('utf-8')
+
 # simulate 
 @mpesa.route("/simulate")
 def simulate():
@@ -109,14 +116,36 @@ def simulate():
 
     request_body = {
 
-        "ShortCode": "600992",
-        "CommandID": "CustomerPayBillOnline",
-        "BillRefNumber": "TestPay1",
-        "Msisdn": "254708374149",
-        "Amount":100
+            "BusinessShortCode":business_short_code,    
+            "Password": "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMTYwMjE2MTY1NjI3",    
+            "Timestamp":formatted_time,    
+            "TransactionType": "CustomerPayBillOnline",    
+            "Amount":"1",    
+            "PartyA":"254708176232",    
+            "PartyB":business_short_code,    
+            "PhoneNumber":"254708176232",    
+            "CallBackURL":"https://darajambili.herokuapp.com/express-payment",    
+            "AccountReference":"Test",    
+            "TransactionDesc":"Pay Hostel Booking Fee"
+
+            # "BusinessShortCode":business_short_code,    
+            # "Password":decoded_string,    
+            # "Timestamp":formatted_time,    
+            # "TransactionType": "CustomerPayBillOnline",    
+            # "Amount":"1",    
+            # "PartyA":"254708374149",    
+            # "PartyB":"174379",    
+            # "PhoneNumber":"254708374149",    
+            # "CallBackURL":"https://darajambili.herokuapp.com/express-payment",    
+            # "AccountReference":"Test",    
+            # "TransactionDesc":"Pay Hostel Booking Fee"
+
     }
-    simulate_response = requests.post(mpesa_endpoint,json=request_body,headers=headers)
-    return simulate_response.json()
+    # simulate_response = requests.post(mpesa_endpoint,json=request_body,headers=headers)
+    # return simulate_response.json()
+
+
+    return 'Hello world'
 
 
 
@@ -126,3 +155,32 @@ def ac_token():
     data =(requests.get(mpesa_auth_url,auth = HTTPBasicAuth(consumer_key,consumer_secret)))
     data = data.json()
     return data['access_token']
+
+
+
+
+
+
+
+
+
+
+    # simulate 
+# @mpesa.route("/simulate")
+# def simulate():
+#     mpesa_endpoint = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate'
+#     access_token = ac_token()
+
+#     headers = { "Authorization ":"Bearer %s" % ac_token()}
+
+#     request_body = {
+
+#         "ShortCode": "600992",
+#         "CommandID": "CustomerPayBillOnline",
+#         "BillRefNumber": "TestPay1",
+#         "Msisdn": "254708374149",
+#         "Amount":100
+#     }
+#     simulate_response = requests.post(mpesa_endpoint,json=request_body,headers=headers)
+#     return simulate_response.json()
+
