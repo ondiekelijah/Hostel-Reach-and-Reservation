526ec110286ab579fb8e9aba2a48319b339cb2d4
Dev-Elie <eochieng9448@gmail.com>
2021-08-17 13:25:24 +0300

    deleted utils

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = '67eadccda3bc198fangelus'
-    SQLALCHEMY_DATABASE_URI = False
--- daraja.py
+++
@@ -1,4 +0,0 @@
-
-import requests
-response = requests.request("GET", 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials', headers = { 'Authorization': 'Bearer cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ==' })
-print(response.text.encode('utf8'))
